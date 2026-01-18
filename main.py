14d420e25e34aaec081b69bee7fb3d75e73492ef
Petre Razvan <petre.ispir2002@protonmail.com>
2026-01-18 19:34:55 +0200

    the main python script

---
+++ main.py
@@ -0,0 +1,13 @@
+from clients import clients
+
+if __name__ == "__main__":
+    clients("data/clients.json", "CLIENT", "1", "0770 420 690", "ORAS", True)
+    clients("data/clients.json", "CLIENT", "2", "0770 420 690", "ORAS", True)
+    clients("data/clients.json", "CLIENT", "3", "0770 420 690", "ORAS", True)
+    clients("data/clients.json", "CLIENT", "4", "0770 420 690", "ORAS", True)
+    clients("data/clients.json", "CLIENT", "5", "0770 420 690", "ORAS", True)
+    clients("data/clients.json", "CLIENT", "6", "0770 420 690", "ORAS", True)
+    clients("data/clients.json", "CLIENT", "7", "0770 420 690", "ORAS", True)
+    clients("data/clients.json", "CLIENT", "8", "0770 420 690", "ORAS", True)
+    clients("data/clients.json", "CLIENT", "9", "0770 420 690", "ORAS", True)
+    clients("data/clients.json", "CLIENT", "10", "0770 420 690", "ORAS", True)
