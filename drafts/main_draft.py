ac428e6f1c25ac51771fc3b911bc10af3653dee5
Petre Razvan <petre.ispir2002@protonmail.com>
2026-01-22 23:20:22 +0200

    Update main_draft.py

--- drafts/main_draft.py
+++ drafts/main_draft.py
@@ -15,59 +15,59 @@ if __name__ == "__main__":
         "(475) 651-6147",
         "Bridgeport, CT",
     )
-    clients_draft(
-        "drafts/data/clients_draft.json",
-        "Marty",
-        "Koch",
-        "(681) 518-7418",
-        "Charleston, WV",
-    )
-    clients_draft(
-        "drafts/data/clients_draft.json",
-        "Billie",
-        "Forbes",
-        "(570) 542-1228",
-        "Scranton, PA",
-    )
-    clients_draft(
-        "drafts/data/clients_draft.json",
-        "Deloris",
-        "White",
-        "(386) 167-3182",
-        "Palm Coast, FL",
-    )
-    clients_draft(
-        "drafts/data/clients_draft.json",
-        "Sherwood",
-        "Holder",
-        "(574) 347-8989",
-        "Elkhart, IN",
-    )
-    clients_draft(
-        "drafts/data/clients_draft.json",
-        "Tobias",
-        "Snyder",
-        "(502) 947-8597",
-        "Louisville, KY",
-    )
-    clients_draft(
-        "drafts/data/clients_draft.json",
-        "Mattie",
-        "Garcia",
-        "(270) 921-6744",
-        "Henderson, KY",
-    )
-    clients_draft(
-        "drafts/data/clients_draft.json",
-        "Hugh",
-        "Maxwell",
-        "(530) 532-3263",
-        "Davis, CA",
-    )
-    clients_draft(
-        "drafts/data/clients_draft.json",
-        "Diana",
-        "Acosta",
-        "(724) 410-6441",
-        "Mccandless, PA",
-    )
+    # clients_draft(
+    #     "drafts/data/clients_draft.json",
+    #     "Marty",
+    #     "Koch",
+    #     "(681) 518-7418",
+    #     "Charleston, WV",
+    # )
+    # clients_draft(
+    #     "drafts/data/clients_draft.json",
+    #     "Billie",
+    #     "Forbes",
+    #     "(570) 542-1228",
+    #     "Scranton, PA",
+    # )
+    # clients_draft(
+    #     "drafts/data/clients_draft.json",
+    #     "Deloris",
+    #     "White",
+    #     "(386) 167-3182",
+    #     "Palm Coast, FL",
+    # )
+    # clients_draft(
+    #     "drafts/data/clients_draft.json",
+    #     "Sherwood",
+    #     "Holder",
+    #     "(574) 347-8989",
+    #     "Elkhart, IN",
+    # )
+    # clients_draft(
+    #     "drafts/data/clients_draft.json",
+    #     "Tobias",
+    #     "Snyder",
+    #     "(502) 947-8597",
+    #     "Louisville, KY",
+    # )
+    # clients_draft(
+    #     "drafts/data/clients_draft.json",
+    #     "Mattie",
+    #     "Garcia",
+    #     "(270) 921-6744",
+    #     "Henderson, KY",
+    # )
+    # clients_draft(
+    #     "drafts/data/clients_draft.json",
+    #     "Hugh",
+    #     "Maxwell",
+    #     "(530) 532-3263",
+    #     "Davis, CA",
+    # )
+    # clients_draft(
+    #     "drafts/data/clients_draft.json",
+    #     "Diana",
+    #     "Acosta",
+    #     "(724) 410-6441",
+    #     "Mccandless, PA",
+    # )
