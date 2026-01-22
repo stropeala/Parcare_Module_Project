94f3485b546cd822e07d604aa709ad121ac3680a
Petre Razvan <petre.ispir2002@protonmail.com>
2026-01-22 23:19:59 +0200

    Update countdown_draft.py

--- drafts/countdown_draft.py
+++ drafts/countdown_draft.py
@@ -1,16 +1,22 @@
 import datetime
 import time
+from random import randint
 
 
-def countdown_draft(t=5):
+def countdown_draft():
+    start_s = str(datetime.datetime.now())
     start = {"Intrare in parcare": str(datetime.datetime.now())}
 
+    t = randint(0, 2)
     while t:
         time.sleep(1)
         t -= 1
 
+    end_s = str(datetime.datetime.now())
     end = {"Parcarea a expirat": str(datetime.datetime.now())}
 
     timp = start | end
 
-    return timp
+    print(type(end_s))
+    print(timp)
+    print(eval(f"{end_s} - {start_s}"))
