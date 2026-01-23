346f06f4c42c0755a8a57f48840ecc98244468a7
Petre Razvan <petre.ispir2002@protonmail.com>
2026-01-23 11:26:13 +0200

    a little cleanup

--- db.py
+++ db.py
@@ -8,14 +8,13 @@ import time
 
 # We create a func that stores the timetable data for each client
 def db(filepath, id):
-    db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/client_id{id}_timer.json"
+    db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/client_timers/client_id{id}_timer.json"
     if os.path.isfile(db_filepath) is True:
         with open(db_filepath, "r") as file:
             client_db = json.load(file)
     else:
         client_db = {}
 
-    global intrare
     intrare = datetime.datetime.now()
     client_db_dict_start = {
         "ID": id,
@@ -29,7 +28,47 @@ def db(filepath, id):
         json.dump(db_start_dict, file, indent=4)
 
     ore = random.randint(1, 24 * 5)
-    if 2 <= ore < 24 * 3:
+    if 1 <= ore <= 2:
+        db_filepath_u2hours = (
+            f"{pathlib.Path(filepath).parent.resolve()}/clients_timer_under_2hours.json"
+        )
+        if os.path.isfile(db_filepath_u2hours) is True:
+            with open(db_filepath_u2hours, "r") as file:
+                client_db_u2hours = json.load(file)
+        else:
+            client_db_u2hours = []
+
+        t = ore
+        while t:
+            time.sleep(1)
+            t -= 1
+
+        iesire = datetime.datetime.now()
+        client_db_dict_u2hours = {
+            "ID": id,
+            "Date & Hour": {
+                "Intrare in parcare": str(intrare),
+                "Iesire din parcare": str(iesire),
+                "Timp": f"{ore} ore",
+            },
+        }
+
+        client_db_u2hours.append(client_db_dict_u2hours)
+        with open(db_filepath_u2hours, "w") as file:
+            # indent=4 for better farmat
+            json.dump(client_db_u2hours, file, indent=4)
+
+        db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/client_timers/client_id{id}_timer.json"
+        with open(db_filepath, "r") as file:
+            client_db = json.load(file)
+
+        client_db["Date & Hour"]["Iesire din parcare"] = str(iesire)
+        client_db["Date & Hour"]["Timp"] = f"{ore} ore"
+        with open(db_filepath, "w") as file:
+            # indent=4 for better farmat
+            json.dump(client_db, file, indent=4)
+
+    elif 2 < ore < 24 * 3:
         db_filepath_2hours = (
             f"{pathlib.Path(filepath).parent.resolve()}/clients_timer_above_2hours.json"
         )
@@ -59,10 +98,7 @@ def db(filepath, id):
             # indent=4 for better farmat
             json.dump(client_db_2hours, file, indent=4)
 
-        db_filepath = (
-            f"{pathlib.Path(filepath).parent.resolve()}/client_id{id}_timer.json"
-        )
-
+        db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/client_timers/client_id{id}_timer.json"
         with open(db_filepath, "r") as file:
             client_db = json.load(file)
 
@@ -72,7 +108,7 @@ def db(filepath, id):
             # indent=4 for better farmat
             json.dump(client_db, file, indent=4)
 
-    elif 72 <= ore:
+    elif 24 * 3 <= ore:
         db_filepath_3days = (
             f"{pathlib.Path(filepath).parent.resolve()}/clients_timer_above_3days.json"
         )
@@ -102,9 +138,7 @@ def db(filepath, id):
             # indent=4 for better farmat
             json.dump(client_db_3days, file, indent=4)
 
-        db_filepath = (
-            f"{pathlib.Path(filepath).parent.resolve()}/client_id{id}_timer.json"
-        )
+        db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/client_timers/client_id{id}_timer.json"
         with open(db_filepath, "r") as file:
             client_db = json.load(file)
 
@@ -115,9 +149,7 @@ def db(filepath, id):
             json.dump(client_db, file, indent=4)
 
     else:
-        db_filepath = (
-            f"{pathlib.Path(filepath).parent.resolve()}/client_id{id}_timer.json"
-        )
+        db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/client_timers/client_id{id}_timer.json"
         with open(db_filepath, "r") as file:
             client_db = json.load(file)
 
