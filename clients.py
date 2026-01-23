83e00e9f4bbde5ad6d118c89623cd324c1abbdf5
Petre Razvan <petre.ispir2002@protonmail.com>
2026-01-23 13:59:35 +0200

    final version (i hope)

--- clients.py
+++ clients.py
@@ -1,5 +1,7 @@
 import json
 import os
+import pathlib
+import re
 
 from db import db
 
@@ -28,10 +30,34 @@ def clients(filepath, nume, prenume, telefon, oras, pariah=False):
 
     # We add the new client dict to the all_clients list
     all_clients.append(client)
-
-    # We open the json in write mode and convert the all_clients list to a json
+    # We open the json in write mode and dump the all_clients list containig the dicts to a json
     with open(filepath, "w") as file:
-        # indent=4 for better farmat
+        # indent=4 for better format
         json.dump(all_clients, file, indent=4)
 
+    # We call the func that sorts the data
     db(filepath, id)
+
+    # We open the json with all the clients timers that we made
+    # and open it in read mode to extract the list
+    db_filepath2 = (
+        f"{pathlib.Path(filepath).parent.resolve()}/client_timers/client_timers.json"
+    )
+    with open(db_filepath2, "r") as file:
+        client_db2 = json.load(file)
+
+    # We make a for loop for each dict in the list
+    # to check the timer and change tha pariah value if its over the "3 days" threshold
+    ids = list(range(len(all_clients)))
+    for i in ids:
+        timp_str = client_db2[i]["Date & Hour"]["Timp"]
+        match = re.findall(r"\d", timp_str)
+        timp_int = int(match[0])
+        if 7 <= timp_int:
+            # We update the dict with True
+            all_clients[i].update({"Pariah": True})
+
+    # Now we open the same file to overwrite with our new list
+    with open(filepath, "w") as file:
+        # indent=4 for better format
+        json.dump(all_clients, file, indent=4)
--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from clients import clients
 
+"""
+Pentru a fi mai usor de aplicat si de inteles singurele modificari pe care le am facut de cerinta sunt:
+    in loc de 72 de ore am limitat timpul la 10 secunde
+
+    in json uri la timp, 1 sec este echivalenta cu o ora
+
+    clientii care stau mai putin sau egal cu 3 secunde sunt cei care stau "sub 2 ore" in parcare:
+        timp <=3 sec => 2 hr
+
+    clientii care stau intre 3 si 7 secunde sunt cei care "depasesc 2 ore":
+        3 sec < timp < 7 sec => 2+ ore
+
+    clientii care stau peste 7 secunde sunt cei care stau "3 zile":
+        7 sec <= timp => 3 zile
+
+    clientii care au depasit 7 secunde a.k.a. "3 zile" li se va schimba pariah din False in True
+
+In rest totul este exact ca si in cerinta de la proiect (sper)
+Am incercat sa explic cat mai bine tot ce am facut
+Sper ca o sa intelegeti, nu sunt cel mai bun la limba romana :)
+
+In drafts sunt toate ciornele mele, nothing important
+"""
+
 if __name__ == "__main__":
     clients(
         "data/clients.json",
